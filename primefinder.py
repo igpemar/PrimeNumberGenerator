3596baf423b10d8c3b4230712d5530c4084106b9
Ignacio V Perez <igp@lunar.app>
2022-05-23 20:43:09 +0200

    added sive of Surnaman and up to 30k optimization for prime finding

--- primefinder.py
+++ primefinder.py
@@ -1,7 +1,10 @@
+import time
 import math
 
 
 def first_N_prime_numbers(N: int, print_flag: bool = False) -> list:
+    """This function provides the first N prime numbers using the
+    up to N."""
     primes = [0] * N
     if N <= 0:
         printif(primes, print_flag)
@@ -21,21 +24,55 @@ def first_N_prime_numbers(N: int, print_flag: bool = False) -> list:
         return primes
 
 
-def prime_numbers_smaller_than_N(N: int, print_flag: bool = False):
-    primes = [True] * N
+def prime_numbers_smaller_than_N_sieve_Eratho(N: int, print_flag: bool = False):
+    """This function provides all the prime numbers in the range [0;N] using
+    the sieve of Erathosthenes algorithm."""
+    if N < 2:
+        return []
+    A = [True] * (N + 1)
     for i in range(2, math.floor(math.sqrt(N)) + 1):
-        if primes[i]:
+        if A[i]:
             j = i**2
-            while j <= N - 1:
-                primes[j] = False
+            while j <= N:
+                A[j] = False
                 j = j + i
-    A, idx = [0] * primes[2:].count(True), 0
-    for i in range(2, N):
-        if primes[i]:
-            A[idx] = i
+    primes, idx = [0] * A[2:].count(True), 0
+    for i in range(2, N + 1):
+        if A[i]:
+            primes[idx] = i
             idx += 1
 
-    return A
+    return primes
+
+
+def prime_numbers_smaller_than_N_sieve_Sundaram(n: int, print_flag: bool = False):
+    """This function provides all the prime numbers in the range [0;N] using
+    the sieve of Sundaram algorithm."""
+    if n < 2:
+        return []
+    elif n == 2:
+        return [2]
+
+    k = math.floor((n - 1) / 2)
+    A = [True] * (k + 1)
+    for i in range(1, math.floor(math.sqrt(k)) + 1):
+        if A[i]:
+            j = i
+            while i + j + 2 * i * j <= k:
+                A[i + j + 2 * i * j] = False
+                j += 1
+            # end
+    # end
+    primes, idx = [0] * (A.count(True)), 1
+    primes[0] = 2
+    printif(primes[0], print_flag)
+    for i in range(1, (k + 1)):
+        if A[i]:
+            primes[idx] = 2 * i + 1
+            printif(primes[idx], print_flag)
+            idx += 1
+
+    return primes
 
 
 def printif(value, print_flag: bool = False):
@@ -53,8 +90,96 @@ def is_prime(number: int) -> bool:
     return True
 
 
-def is_all_array_prime(primes):
+def is_prime_2k(number: int) -> bool:
+    if number <= 3:
+        return number > 1
+    if not number % 2:
+        return False
+    max_div = math.floor(math.sqrt(number))
+    for divider in range(3, max_div + 1, 2):
+        if number % divider == 0:
+            return False
+    return True
+
+
+def is_prime_6k(number: int) -> bool:
+    if number <= 3:
+        return number > 1
+    if not number % 2 or not number % 3:
+        return False
+    max_div = math.floor(math.sqrt(number))
+    for divider in range(5, max_div + 1, 6):
+        if not number % divider or not number % (divider + 2):
+            return False
+    return True
+
+
+def is_prime_30k(number: int) -> bool:
+    if number <= 3:
+        return number > 1
+    elif number == 5:
+        return number
+    if not number % 2 or not number % 3 or not number % 5:
+        return False
+    max_div = math.floor(math.sqrt(number))
+    for divider in range(7, max_div + 1, 30):
+        if (
+            not number % divider
+            or not number % (divider + 2)
+            or not number % (divider + 4)
+            or not number % (divider + 10)
+            or not number % (divider + 12)
+            or not number % (divider + 16)
+            or not number % (divider + 22)
+            or not number % (divider + 24)
+        ):
+            return False
+    return True
+
+
+def is_all_array_primes(primes, print_flag: bool = False):
+    start = time.time()
     for prime in primes:
         if not is_prime(prime):
+            if print_flag:
+                print("--> Check time: " + str(round(time.time() - start, 4)) + " s.")
             return False
+    if print_flag:
+        print("--> Check time: " + str(round(time.time() - start, 4)) + " s.")
+    return True
+
+
+def is_all_array_primes_2k(primes, print_flag: bool = False):
+    start = time.time()
+    for prime in primes:
+        if not is_prime_2k(prime):
+            if print_flag:
+                print("--> Check time: " + str(round(time.time() - start, 4)) + " s.")
+            return False
+    if print_flag:
+        print("--> Check time: " + str(round(time.time() - start, 4)) + " s.")
+    return True
+
+
+def is_all_array_primes_6k(primes, print_flag: bool = False):
+    start = time.time()
+    for prime in primes:
+        if not is_prime_6k(prime):
+            if print_flag:
+                print("--> Check time: " + str(round(time.time() - start, 4)) + " s.")
+            return False
+    if print_flag:
+        print("--> Check time: " + str(round(time.time() - start, 4)) + " s.")
+    return True
+
+
+def is_all_array_primes_30k(primes, print_flag: bool = False):
+    start = time.time()
+    for prime in primes:
+        if not is_prime_30k(prime):
+            if print_flag:
+                print("--> Check time: " + str(round(time.time() - start, 4)) + " s.")
+            return False
+    if print_flag:
+        print("--> Check time: " + str(round(time.time() - start, 4)) + " s.")
     return True
--- test.py
+++ test.py
@@ -1,19 +1,17 @@
 import time
-from primefinder import (
-    first_N_prime_numbers,
-    prime_numbers_smaller_than_N,
-    is_all_array_prime,
-)
+from primefinder import *
 
 N = 1000
-print(f"Finding the first {N} prime numbers by brute force ...")
+print(f"Finding the first {N} prime numbers...")
+print("Algorithm: old brute force.")
 start_1 = time.time()
 primes_1 = first_N_prime_numbers(N, False)
 end_1 = time.time()
 
+
 print("Elapsed time: " + str(round(end_1 - start_1, 3)) + " s.")
-if is_all_array_prime(primes_1):
-    print("Checking results...")
+print("Checking results with 30k optimization...")
+if is_all_array_primes_30k(primes_1, True):
     print("---> Array is entirely prime.")
     print(f"Last 5 primes: {primes_1[-5:]}")
     print(f"Prime density: {round(len(primes_1)/primes_1[-1]*100,1)} %")
@@ -21,17 +19,35 @@ else:
     print("ARRAY NOT PRIME.")
 print("\n")
 
-N = 10000000
+N = 1000000
 print(f"Printing all the the prime numbers up to {N} ...")
+print("Algorithm: Sieve of Eratosthenes.")
 start_2 = time.time()
-primes_2 = prime_numbers_smaller_than_N(N, False)
+primes_2 = prime_numbers_smaller_than_N_sieve_Eratho(N, False)
 end_2 = time.time()
-print("Elapsed time: " + str(round(end_2 - start_2, 3)) + " s.")
-if is_all_array_prime(primes_2):
-    print("Checking results...")
+print("Elapsed time: " + str(round(end_2 - start_2, 4)) + " s.")
+print("Checking results with 30k optimization...")
+if is_all_array_primes_30k(primes_2, True):
     print("---> Array is entirely prime.")
     print(f"Last 5 primes: {primes_2[-5:]}")
     print(f"Total primes found: {len(primes_2)}.")
     print(f"Prime density: {round(len(primes_2)/N*100,1)} %")
 else:
     print("ARRAY NOT PRIME.")
+print("\n")
+
+print(f"Printing all the the prime numbers up to {N} ...")
+print("Algorithm: Sieve of Sundaram.")
+start_3 = time.time()
+primes_3 = prime_numbers_smaller_than_N_sieve_Sundaram(N, False)
+end_2 = time.time()
+print("Elapsed time: " + str(round(end_2 - start_3, 4)) + " s.")
+print("Checking results with 30k optimization...")
+if is_all_array_primes_30k(primes_3, True):
+    print("---> Array is entirely prime.")
+    print(f"Last 5 primes: {primes_3[-5:]}")
+    print(f"Total primes found: {len(primes_3)}.")
+    print(f"Prime density: {round(len(primes_3)/N*100,1)} %")
+else:
+    print("ARRAY NOT PRIME.")
+print("\n")
